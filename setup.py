52c39231ef41dcf02473aeb6676ae88ea48327e2
Derrick Petzold <github@petzold.io>
2019-06-08 15:46:20 -0700

    packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
+import pathlib
 from setuptools import setup
 
 setup(
     name='aws-log-parser',
     version='1.3.1',
     description='Python module for parsing AWS CloudFront and LoadBalancer logs',
+    long_description=(pathlib.Path(__file__).parent / 'README.md').read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dpetzold/aws-log-parser',
+    author='Derrick Petzold',
+    author_email='github@petzold.io',
+    license='Apache',
     packages=[
         'aws_log_parser',
     ],
