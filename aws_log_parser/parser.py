b04b2fa06c385017e9060374a8e4324968abf8f5
Derrick Petzold <github@petzold.io>
2019-08-20 16:13:55 -0700

    ...

--- aws_log_parser/parser.py
+++ aws_log_parser/parser.py
@@ -11,7 +11,8 @@ def to_python(value, field):
 def log_parser(content, log_type):
     fields = dataclasses.fields(log_type.model)
     for row in csv.reader(content, delimiter=log_type.delimiter):
-        if not row[0].startswith('#'):
-            yield log_type.model(*[
-                to_python(value, field) for value, field in zip(row, fields)
-            ])
+        if not row or row[0].startswith('#'):
+            continue
+        yield log_type.model(*[
+            to_python(value, field) for value, field in zip(row, fields)
+        ])
