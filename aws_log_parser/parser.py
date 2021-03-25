5f0ca737d7d9066213ef9a85404601d85c08c8ef
Derrick Petzold <github@petzold.io>
2021-03-24 19:55:12 -0700

    Use urllib.parse.unquote
    To decode the cookie's key

--- aws_log_parser/parser.py
+++ aws_log_parser/parser.py
@@ -49,13 +49,7 @@ def to_http_request(value):
 def to_cookie(value):
     cookie = cookies.SimpleCookie()
     cookie.load(rawdata=value)
-
-    cookiedict = {}
-
-    for key, morsel in cookie.items():
-        cookiedict[key.replace("%20", "")] = morsel.value
-
-    return cookiedict
+    return {urllib.parse.unquote(key): morsel.value for key, morsel in cookie.items()}
 
 
 def to_python(value, field):
