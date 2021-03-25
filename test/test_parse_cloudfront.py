9372f613d556a9a632e719d70aeaff896b1b0ec9
Derrick Petzold <github@petzold.io>
2021-03-24 20:32:15 -0700

    Use replace

--- test/test_parse_cloudfront.py
+++ test/test_parse_cloudfront.py
@@ -1,4 +1,5 @@
 import datetime
+import dataclasses
 
 
 from .conftest import parse_entry
@@ -43,24 +44,22 @@ def test_cloudfront_entry_cookie_with_encoding(
 def test_cloudfront_entry2(
     base_cloudfront_log_entry, cloudfront_entry2, cookie_zip_code
 ):
-
-    base_cloudfront_log_entry.time = datetime.time(1, 13, 12)
-    base_cloudfront_log_entry.edge_location = "LAX1"
-    base_cloudfront_log_entry.sent_bytes = 2390282
-    base_cloudfront_log_entry.cookie = cookie_zip_code
-    base_cloudfront_log_entry.client_ip = "192.0.2.202"
-    base_cloudfront_log_entry.uri_stream = "/soundtrack/happy.mp3"
-    base_cloudfront_log_entry.status_code = 304
-    base_cloudfront_log_entry.referrer = "www.unknownsingers.com"
-    base_cloudfront_log_entry.uri_query = {"a": ["b"], "c": ["d"]}
-    base_cloudfront_log_entry.edge_result_type = "Hit"
-    base_cloudfront_log_entry.edge_response_result_type = "Hit"
-    base_cloudfront_log_entry.time_taken = 0.002
-    base_cloudfront_log_entry.user_agent = (
-        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)"
-    )
-    base_cloudfront_log_entry.edge_request_id = (
-        "xGN7KWpVEmB9Dp7ctcVFQC4E-nrcOcEKS3QyAez--06dV7TEXAMPLE=="
+    cloudfront_log_entry2 = dataclasses.replace(
+        base_cloudfront_log_entry,
+        time=datetime.time(1, 13, 12),
+        edge_location="LAX1",
+        sent_bytes=2390282,
+        cookie=cookie_zip_code,
+        client_ip="192.0.2.202",
+        uri_stream="/soundtrack/happy.mp3",
+        status_code=304,
+        referrer="www.unknownsingers.com",
+        uri_query={"a": ["b"], "c": ["d"]},
+        edge_result_type="Hit",
+        edge_response_result_type="Hit",
+        time_taken=0.002,
+        user_agent="Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+        edge_request_id="xGN7KWpVEmB9Dp7ctcVFQC4E-nrcOcEKS3QyAez--06dV7TEXAMPLE==",
     )
     entry = parse_entry(cloudfront_entry2, LogType.CloudFront)
-    assert entry == base_cloudfront_log_entry
+    assert entry == cloudfront_log_entry2
