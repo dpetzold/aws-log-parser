11fae4e9fdaca0f6772a76c45c08d303005ea6dd
Derrick Petzold <github@petzold.io>
2021-11-03 10:28:38 -0700

    s3 support

--- aws_log_parser/cli.py
+++ aws_log_parser/cli.py
@@ -17,11 +17,36 @@ ec2_client = boto3.client("ec2")
 s3_client = boto3.client("s3")
 
 
-def parse_file(path):
+def s3_list_files(bucket, prefix, sort_key, reverse=True):
+
+    paginator = s3_client.get_paginator("list_objects_v2",).paginate(
+        Bucket=bucket,
+        Prefix=prefix,
+    )
+
+    items = [item for page in paginator for item in page["Contents"]]
+
+    return sorted(items, key=lambda x: x[sort_key], reverse=reverse)
+
+
+def read_file(path):
     with open(path) as log_data:
         yield from AwsLogParser(
             log_type=LogType.ClassicLoadBalancer,
         ).parse(log_data.readlines())
+
+
+def read_s3(bucket, prefix):
+    parser = AwsLogParser(log_type=LogType.ClassicLoadBalancer)
+
+    files = s3_list_files(bucket, prefix, "LastModified")
+
+    for file in files:
+        print(f"{bucket}/{file['Key']}")
+        contents = s3_client.get_object(Bucket=bucket, Key=file["Key"])
+        yield from parser.parse(
+            [line.decode("utf-8") for line in contents["Body"].iter_lines()]
+        )
 
 
 def get_tag(tags, name):
@@ -95,5 +120,12 @@ def main():
     parsed = urlparse(args.url)
 
     if parsed.scheme == "file":
-        entries = parse_file(parsed.path)
-        count_hosts(entries)
+        entries = read_file(parsed.path)
+
+    elif parsed.scheme == "s3":
+        entries = read_s3(parsed.netloc, parsed.path.lstrip("/"))
+
+    else:
+        raise ValueError(f"Unknown scheme {parsed.scheme}")
+
+    count_hosts(entries)
