923c39fd0e6be900b7eedd09cc24caee53291d6f
Derrick Petzold <github@petzold.io>
2021-11-27 19:40:26 -0800

    85% coverage

--- test/test_interface.py
+++ test/test_interface.py
@@ -1,27 +1,41 @@
+import pytest
+
 from aws_log_parser import (
     AwsLogParser,
     LogType,
 )
 
 
-def test_read_file():
-    AwsLogParser(LogType.ClassicLoadBalancer).read_file(
-        "test/data/cloudfront-multiple.log"
+@pytest.fixture
+def cloudfront_parser():
+    return AwsLogParser(
+        log_type=LogType.CloudFront,
+        profile="personal",
+        region="us-west-2",
     )
 
 
-def test_read_files():
-    AwsLogParser(LogType.ClassicLoadBalancer).read_files("test/data")
+def test_read_file(cloudfront_parser):
+    entries = cloudfront_parser.read_file("test/data/cloudfront-multiple.log")
+
+    assert len(list(entries)) == 6
 
 
-def test_read_s3():
-    AwsLogParser(LogType.ClassicLoadBalancer).read_s3(
+def test_read_files(cloudfront_parser):
+    entries = cloudfront_parser.read_files(["test/data/cloudfront-multiple.log"])
+    assert len(list(entries)) == 6
+
+
+def test_read_s3(cloudfront_parser):
+    entries = cloudfront_parser.read_s3(
         "aws-logs-test-data",
         "cloudfront-multiple.log",
     )
+    assert len(list(entries)) == 6
 
 
-def test_read_url():
-    AwsLogParser(LogType.ClassicLoadBalancer).read_url(
+def test_read_url(cloudfront_parser):
+    entries = cloudfront_parser.read_url(
         "s3://aws-logs-test-data/cloudfront-multiple.log"
     )
+    assert len(list(entries)) == 6
