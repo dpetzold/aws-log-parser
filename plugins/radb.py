cc27d26dadc0c2d8ab9238ab813e14508575635d
Derrick Petzold <github@petzold.io>
2022-01-16 09:21:02 -0800

    final

--- plugins/radb.py
+++ plugins/radb.py
@@ -2,10 +2,8 @@ import logging
 import socket
 
 from dataclasses import dataclass
-from pprint import pprint
 
 import whois
-
 
 from aws_log_parser.plugin import AwsLogParserPlugin
 from aws_log_parser.util import gethostbyname
@@ -15,11 +13,13 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class RadbPlugin(AwsLogParserPlugin):
+    """
+    Get the public network name from RADB.
+    """
+
     attr_name: str = "network"
 
     def parse_response(self, response):
-        pprint(response)
-
         d = {}
         for line in response.split("\n"):
             if line == "":
@@ -54,8 +54,6 @@ class RadbPlugin(AwsLogParserPlugin):
     def augment(self, log_entries):
 
         networks = self.lookup({log_entry.client_ip for log_entry in log_entries})
-
-        pprint(networks)
 
         for log_entry in log_entries:
             setattr(log_entry, self.attr_name, networks.get(log_entry.client_ip))
