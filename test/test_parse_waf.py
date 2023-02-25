5e2a606435e0d17fbe6cc3eead7aee825ec2e554
Derrick Petzold <github@petzold.io>
2023-02-24 20:03:36 -0800

    wip

---
+++ test/test_parse_waf.py
@@ -0,0 +1,20 @@
+import datetime
+import json
+import typing
+
+from .conftest import parse_entry
+
+from aws_log_parser.models import (
+    LogType,
+    WafLogEntry,
+)
+
+from pprint import pprint
+
+
+def test_waf_entry(waf_entry):
+    waf_entry = typing.cast(
+        WafLogEntry, parse_entry([json.loads(waf_entry.read())], LogType.WAF)
+    )
+    pprint(waf_entry)
+    assert isinstance(waf_entry.timestamp, datetime.datetime) is True
