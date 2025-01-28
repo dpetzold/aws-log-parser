d22a885f0609300737eae68ca007227554186eea
Derrick Petzold <github@petzold.io>
2025-01-28 12:45:25 -0800

    Fix error:
    TypeError: a bytes-like object is required, not 'generator'

--- test/test_interface.py
+++ test/test_interface.py
@@ -28,7 +28,14 @@ class MockPaginator:
                     "ETag": '"37c13f9a66a79c2b474356adaf5da1d0"',
                     "Size": 2844,
                     "StorageClass": "STANDARD",
-                }
+                },
+                {
+                    "Key": f"csv-file.csv{suffix}",
+                    "LastModified": datetime.datetime(2021, 11, 28, 3, 31, 56, tzinfo=tzutc()),
+                    "ETag": '"37c13f9a66a79c2b474356adaf555555"',
+                    "Size": 2844,
+                    "StorageClass": "STANDARD",
+                },
             ],
         }
 
@@ -61,6 +68,16 @@ def cloudfront_parser():
     return AwsLogParser(
         log_type=LogType.CloudFront,
     )
+
+
+def test_regex_filter(monkeypatch):
+    monkeypatch.setattr(S3Service, "client", MockS3Client())
+    aws_log_parser = AwsLogParser(
+        log_type=LogType.CloudFront,
+        regex_filter="cloudfront",
+    )
+    entries = aws_log_parser.read_url("s3://aws-logs-test-data/")
+    assert len(list(entries)) == 6
 
 
 def test_parse_file(cloudfront_parser):
